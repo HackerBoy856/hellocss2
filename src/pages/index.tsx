2b9f1222322898500b7acdb16a610791621865ce
venvishuang <809737487@qq.com>
2022-04-07 11:38:30 +0800

    4

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+// @ts-ignore
+import React,{ FC } from 'react'
+import "../style/index.scss"
+import Header from "./header";
+
+interface IndexProps {
+}
+
+const Index: FC<IndexProps> = props => {
+  const {
+  }=props
+  return (
+    <>
+      <Header />
+
+
+    </>
+
+  )
+}
+
+export default Index;
